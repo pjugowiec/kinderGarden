595aaaa870844c20106b62f2eb9dde0d5892a6c3
Patryk Jugowiec <patryk.jugowiec@o2.pl>
2021-05-26 18:34:52 +0200

    Added TestContainers

--- server/src/main/java/com/raport/services/mappers/UserMapper.java
+++ server/src/main/java/com/raport/services/mappers/UserMapper.java
@@ -1,28 +1,28 @@
-package com.raport.services.mappers;
-
-import com.raport.domain.entity.UserEntity;
-import com.raport.domain.model.UserDto;
-import org.mapstruct.Mapper;
-
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
-
-import java.util.List;
-
-@Mapper
-public interface UserMapper {
-
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
-    @Mapping(target = "name", ignore = true)
-    UserDto userToUserDto(UserEntity userEntity);
-    List<UserDto> usersToUserDtos(List<UserEntity> userEntities);
-
-//    @Mapping(target = "dates", ignore = true)
-//    @Mapping(target = "id", source = "userId")
-//    UserEntity userDtoToUser(UserDto userDto);
-
-
-//    List<UserEntity> userDtosToUsers(List<UserDto> userDtos);
-
-}
+//package com.raport.services.mappers;
+//
+//import com.raport.domain.entity.UserEntity;
+//import com.raport.domain.model.UserDto;
+//import org.mapstruct.Mapper;
+//
+//import org.mapstruct.Mapping;
+//import org.mapstruct.factory.Mappers;
+//
+//import java.util.List;
+//
+//@Mapper
+//public interface UserMapper {
+//
+//    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+//
+//    @Mapping(target = "name", ignore = true)
+//    UserDto userToUserDto(UserEntity userEntity);
+//    List<UserDto> usersToUserDtos(List<UserEntity> userEntities);
+//
+////    @Mapping(target = "dates", ignore = true)
+////    @Mapping(target = "id", source = "userId")
+////    UserEntity userDtoToUser(UserDto userDto);
+//
+//
+////    List<UserEntity> userDtosToUsers(List<UserDto> userDtos);
+//
+//}
--- server/src/test/java/com/KinderGardenApplicationTests.java
+++ server/src/test/java/com/KinderGardenApplicationTests.java
@@ -1,4 +1,4 @@
-package com.raport;
+package com;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
